214e83a2272a415ba976558ea1c5bc4ad00fa89d
carlitos <carloserocha27@gmail.com>
2022-09-17 19:16:17 -0300

    get products by range

--- product/product.go
+++ product/product.go
@@ -5,24 +5,24 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"strings"
 	"time"
 
 	"github.com/integrmais/bling"
+	"github.com/integrmais/bling/internal"
 )
 
 type ProductService struct {
-	AppKey  string
-	Client  *http.Client
-	Timeout time.Duration
+	AppKey string
+	Client *http.Client
 }
 
-func NewProductService(appKey string, c *http.Client, t time.Duration) *ProductService {
+func NewProductService(appKey string, c *http.Client) *ProductService {
 	return &ProductService{
-		AppKey:  appKey,
-		Client:  c,
-		Timeout: t,
+		AppKey: appKey,
+		Client: c,
 	}
 }
 
@@ -41,21 +41,25 @@ func HandlerError(req ResponseModel) error {
 	)
 }
 
-func HandlerProduct(response *http.Response) (ResponseModel, error) {
-	defer response.Body.Close()
+func HandlerResponse(res *http.Response) (ResponseModel, error) {
+	defer res.Body.Close()
 
-	var products ResponseModel
-	err := json.NewDecoder(response.Body).Decode(&products)
+	body, _ := io.ReadAll(res.Body)
+
+	fmt.Println(string(body))
+
+	var response ResponseModel
+	err := json.NewDecoder(res.Body).Decode(&response)
 	if err != nil {
 		return ResponseModel{}, err
 	}
 
-	err = HandlerError(products)
+	err = HandlerError(response)
 	if err != nil {
 		return ResponseModel{}, err
 	}
 
-	return products, nil
+	return response, nil
 }
 
 func (s *ProductService) GetProductById(ctx context.Context, productID string) (Product, error) {
@@ -67,27 +71,67 @@ func (s *ProductService) GetProductById(ctx context.Context, productID string) (
 	)
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
-
-	q := req.URL.Query()
-
-	q.Add("apikey", s.AppKey)
-	q.Add("imagem", "S")
-
-	req.URL.RawQuery = q.Encode()
-
 	if err != nil {
 		return Product{}, err
 	}
+
+	q := req.URL.Query()
+	q.Add("apikey", s.AppKey)
+	q.Add("imagem", "S")
+	q.Add("situacao", "A")
+	q.Add("tipo", "P")
+
+	req.URL.RawQuery = q.Encode()
 
 	res, err := s.Client.Do(req)
 	if err != nil {
 		return Product{}, err
 	}
 
-	p, err := HandlerProduct(res)
+	p, err := HandlerResponse(res)
 	if err != nil {
 		return Product{}, err
 	}
 
 	return p.Response.Products[0].Product, nil
+}
+
+func (s *ProductService) GetByRange(ctx context.Context, startAt time.Time) ([]Product, error) {
+	url := bling.ProductsUrl + bling.DefaultResponseType
+
+	by := internal.NormalizeDate(startAt)
+	at := internal.NormalizeDate(time.Now())
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	q := req.URL.Query()
+	q.Add("apikey", s.AppKey)
+	q.Add("imagem", "S")
+	q.Add("situacao", "A")
+	q.Add("tipo", "P")
+	q.Add("filters", fmt.Sprintf("dataAlteracao[%s TO %s]", by, at))
+
+	req.URL.RawQuery = q.Encode()
+
+	fmt.Println(req.URL.RawQuery, q)
+
+	res, err := s.Client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	p, err := HandlerResponse(res)
+	if err != nil {
+		return nil, err
+	}
+
+	var products []Product
+	for _, product := range p.Response.Products {
+		products = append(products, product.Product)
+	}
+
+	return products, nil
 }
