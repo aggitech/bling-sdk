45b71f68c51248a939a788a248a7d8d8faf6076e
carlitos <carloserocha27@gmail.com>
2022-09-20 12:43:14 -0300

    add not found exception message

--- bling.go
+++ bling.go
@@ -6,6 +6,7 @@ import (
 
 const DefaultTimeFormat = "02/01/2006"
 const DefaultTimeout = 60 * time.Second
+const DefaultNotFoundException = "A informacao desejada nao foi encontrada"
 const DefaultResponseType = "/json"
 const DefaultUrl = "https://bling.com.br/Api/v2"
 const ProductsUrl = DefaultUrl + "/produtos"
