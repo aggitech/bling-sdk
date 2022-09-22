eef5a2fdd9e90fdb0dc2975da32c549c753512d7
carlitos <carloserocha27@gmail.com>
2022-09-21 23:00:07 -0300

    updating entity

--- product/entity.go
+++ product/entity.go
@@ -34,60 +34,60 @@ type Images struct {
 
 type Product struct {
 	XMLName               xml.Name `xml:"produto"`
-	ID                    string   `json:"id" xml:"id"`
-	Codigo                string   `json:"codigo" xml:"codigo"`
-	Descricao             string   `json:"descricao" xml:"descricao"`
-	Tipo                  string   `json:"tipo" xml:"tipo"`
-	Situacao              string   `json:"situacao" xml:"situacao"`
-	Unidade               string   `json:"unidade" xml:"unidade"`
-	Preco                 string   `json:"preco" xml:"preco"`
-	PrecoCusto            string   `json:"precoCusto" xml:"preco_custo"`
-	VlrUnit               string   `json:"vlr_unit" xml:"vlr_unit"`
-	DescricaoCurta        string   `json:"descricaoCurta" xml:"descricaoCurta"`
-	DescricaoComplementar string   `json:"descricaoComplementar" xml:"descricaoComplementar"`
+	ID                    string   `json:"id" xml:"id,omitempty"`
+	Codigo                string   `json:"codigo" xml:"codigo,omitempty"`
+	Descricao             string   `json:"descricao" xml:"descricao,omitempty"`
+	Tipo                  string   `json:"tipo" xml:"tipo,omitempty"`
+	Situacao              string   `json:"situacao" xml:"situacao,omitempty"`
+	Unidade               string   `json:"unidade" xml:"unidade,omitempty"`
+	Preco                 string   `json:"preco" xml:"preco,omitempty"`
+	PrecoCusto            string   `json:"precoCusto" xml:"preco_custo,omitempty"`
+	VlrUnit               string   `xml:"vlr_unit,omitempty"`
+	DescricaoCurta        string   `json:"descricaoCurta" xml:"descricaoCurta,omitempty"`
+	DescricaoComplementar string   `json:"descricaoComplementar" xml:"descricaoComplementar,omitempty"`
 	DataInclusao          string   `json:"dataInclusao"  xml:",omitempty"`
 	DataAlteracao         string   `json:"dataAlteracao" xml:",omitempty"`
 	ImageThumbnail        string   `json:"imageThumbnail"  xml:",omitempty"`
-	URLVideo              string   `json:"urlVideo" xml:"urlVideo"`
-	NomeFornecedor        string   `json:"nomeFornecedor" xml:"nomeFornecedor"`
-	CodigoFabricante      string   `json:"codigoFabricante" xml:"codigoFabricante"`
-	Marca                 string   `json:"marca" xml:"marca"`
-	ClassFiscal           string   `json:"class_fiscal" xml:"class_fiscal"`
-	Cest                  string   `json:"cest" xml:"cest"`
-	Origem                string   `json:"origem" xml:"origem"`
-	IDGrupoProduto        string   `json:"idGrupoProduto" xml:"idGrupoProduto"`
-	LinkExterno           string   `json:"linkExterno" xml:"linkExterno"`
-	Observacoes           string   `json:"observacoes" xml:"observacoes"`
-	Garantia              string   `json:"garantia" xml:"garantia"`
-	DescricaoFornecedor   string   `json:"descricaoFornecedor" xml:"descricaoFornecedor"`
-	IDFabricante          string   `json:"idFabricante" xml:"idFabricante"`
+	URLVideo              string   `json:"urlVideo" xml:"urlVideo,omitempty"`
+	NomeFornecedor        string   `json:"nomeFornecedor" xml:"nomeFornecedor,omitempty"`
+	CodigoFabricante      string   `json:"codigoFabricante" xml:"codigoFabricante,omitempty"`
+	Marca                 string   `json:"marca" xml:"marca,omitempty"`
+	ClassFiscal           string   `json:"class_fiscal" xml:"class_fiscal,omitempty"`
+	Cest                  string   `json:"cest" xml:"cest,omitempty"`
+	Origem                string   `json:"origem" xml:"origem,omitempty"`
+	IDGrupoProduto        string   `json:"idGrupoProduto" xml:"idGrupoProduto,omitempty"`
+	LinkExterno           string   `json:"linkExterno" xml:"linkExterno,omitempty"`
+	Observacoes           string   `json:"observacoes" xml:"observacoes,omitempty"`
+	Garantia              string   `json:"garantia" xml:"garantia,omitempty"`
+	DescricaoFornecedor   string   `json:"descricaoFornecedor" xml:"descricaoFornecedor,omitempty"`
+	IDFabricante          string   `json:"idFabricante" xml:"idFabricante,omitempty"`
 	Categoria             Category `json:"categoria"  xml:",omitempty"`
-	IDCategoria           string   `json:"idCategoria" xml:"idCategoria"`
-	PesoLiq               string   `json:"pesoLiq" xml:"peso_liq"`
-	PesoBruto             string   `json:"pesoBruto" xml:"peso_bruto"`
-	EstoqueMinimo         string   `json:"estoqueMinimo" xml:"estoqueMinimo"`
-	EstoqueMaximo         string   `json:"estoqueMaximo" xml:"estoqueMaximo"`
-	Gtin                  string   `json:"gtin" xml:"gtin"`
-	GtinEmbalagem         string   `json:"gtinEmbalagem" xml:"gtinEmbalagem"`
-	LarguraProduto        string   `json:"larguraProduto" xml:"larguraProduto"`
-	AlturaProduto         string   `json:"alturaProduto" xml:"alturaProduto"`
-	ProfundidadeProduto   string   `json:"profundidadeProduto" xml:"profundidadeProduto"`
-	UnidadeMedida         string   `json:"unidadeMedida" xml:"unidadeMedida"`
-	ItensPorCaixa         int      `json:"itensPorCaixa" xml:"itensPorCaixa"`
-	Volumes               int      `json:"volumes" xml:"volumes"`
-	Localizacao           string   `json:"localizacao" xml:"localizacao"`
-	Crossdocking          string   `json:"crossdocking" xml:"crossdocking"`
-	Condicao              string   `json:"condicao" xml:"condicao"`
-	FreteGratis           string   `json:"freteGratis" xml:"freteGratis"`
-	Producao              string   `json:"producao" xml:"producao"`
-	DataValidade          string   `json:"dataValidade" xml:"dataValidade"`
-	SpedTipoItem          string   `json:"spedTipoItem" xml:"spedTipoItem"`
-	ClonarDadosPai        string   `json:"clonarDadosPai" xml:"clonarDadosPai"`
-	CodigoPai             string   `json:"codigoPai" xml:"codigoPai"`
-	EstoqueAtual          int      `json:"estoqueAtual" xml:"estoqueAtual"`
+	IDCategoria           string   `json:"idCategoria" xml:"idCategoria,omitempty"`
+	PesoLiq               string   `json:"pesoLiq" xml:"peso_liq,omitempty"`
+	PesoBruto             string   `json:"pesoBruto" xml:"peso_bruto,omitempty"`
+	EstoqueMinimo         string   `json:"estoqueMinimo" xml:"estoqueMinimo,omitempty"`
+	EstoqueMaximo         string   `json:"estoqueMaximo" xml:"estoqueMaximo,omitempty"`
+	Gtin                  string   `json:"gtin" xml:"gtin,omitempty"`
+	GtinEmbalagem         string   `json:"gtinEmbalagem" xml:"gtinEmbalagem,omitempty"`
+	LarguraProduto        string   `json:"larguraProduto" xml:"larguraProduto,omitempty"`
+	AlturaProduto         string   `json:"alturaProduto" xml:"alturaProduto,omitempty"`
+	ProfundidadeProduto   string   `json:"profundidadeProduto" xml:"profundidadeProduto,omitempty"`
+	UnidadeMedida         string   `json:"unidadeMedida" xml:"unidadeMedida,omitempty"`
+	ItensPorCaixa         int      `json:"itensPorCaixa" xml:"itensPorCaixa,omitempty"`
+	Volumes               int      `json:"volumes" xml:"volumes,omitempty"`
+	Localizacao           string   `json:"localizacao" xml:"localizacao,omitempty"`
+	Crossdocking          string   `json:"crossdocking" xml:"crossdocking,omitempty"`
+	Condicao              string   `json:"condicao" xml:"condicao,omitempty"`
+	FreteGratis           string   `json:"freteGratis" xml:"freteGratis,omitempty"`
+	Producao              string   `json:"producao" xml:"producao,omitempty"`
+	DataValidade          string   `json:"dataValidade" xml:"dataValidade,omitempty"`
+	SpedTipoItem          string   `json:"spedTipoItem" xml:"spedTipoItem,omitempty"`
+	ClonarDadosPai        string   `json:"clonarDadosPai" xml:"clonarDadosPai,omitempty"`
+	CodigoPai             string   `json:"codigoPai" xml:"codigoPai,omitempty"`
+	EstoqueAtual          int      `json:"estoqueAtual" xml:"estoqueAtual,omitempty"`
 	Depositos             []struct {
-		Warehouse `json:"deposito" xml:"deposito"`
+		Warehouse `json:"deposito"`
 	} `json:"depositos"`
-	Deposito Warehouse `json:"deposito" xml:"deposito"`
-	Imagens  Images    `json:"imagens" xml:"imagens"`
+	Deposito Warehouse `json:"deposito" xml:"deposito,omitempty"`
+	Imagens  Images    `json:"imagens" xml:"imagens,omitempty"`
 }
